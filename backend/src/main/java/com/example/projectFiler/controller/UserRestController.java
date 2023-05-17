62d283e8aa0b3d5e16fe89d647e9ab9c97fe7a7d
aljait03 <aljait03@hs-esslingen.de>
2023-05-17 17:52:27 +0200

    added userservice interface

--- backend/src/main/java/com/example/projectFiler/controller/UserRestController.java
+++ backend/src/main/java/com/example/projectFiler/controller/UserRestController.java
@@ -7,7 +7,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
+import jakarta.validation.Valid;
 import java.util.List;
 
 @RestController
