1a5808c64eec3fed151ce42775e3be0675cf6a17
aljait03 <aljait03@hs-esslingen.de>
2023-05-17 17:52:01 +0200

    javax is deprecated, so changed to jakarta

--- backend/src/main/java/com/example/projectFiler/service/UserService.java
+++ backend/src/main/java/com/example/projectFiler/service/UserService.java
@@ -1,4 +1,18 @@
 package com.example.projectFiler.service;
 
-public class UserService {
+import com.example.projectFiler.dto.UserDto;
+
+import java.util.List;
+
+public interface UserService {
+
+    UserDto save(UserDto userDto);
+
+    UserDto update(UserDto userDto);
+
+    UserDto getById(Long id);
+
+    List<UserDto> getAll();
+
+    void deleteById(Long id);
 }
