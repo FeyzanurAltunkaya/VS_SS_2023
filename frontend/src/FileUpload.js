508f802d7bf3bc9ff79e4abac76dbd5a6a6c5741
tukait02 <tukait02@hs-esslingen.de>
2023-05-24 23:30:28 +0200

    upload speicher, gespeicherte daten anzeigen und delete button hinzugefügt

--- frontend/src/FileUpload.js
+++ frontend/src/FileUpload.js
@@ -1,7 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const FileUpload = () => {
     const [selectedFile, setSelectedFile] = useState(null);
+    const [uploadedFiles, setUploadedFiles] = useState([]);
+
+    const fetchUploadedFiles = () => {
+        fetch('http://localhost:8000/files')
+            .then((response) => response.json())
+            .then((data) => {
+                setUploadedFiles(data.files);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    };
+
+    useEffect(() => {
+        // Hier kannst du eine Funktion aufrufen, um die hochgeladenen Dateien abzurufen
+        fetchUploadedFiles();
+    }, []);
     const handleFileChange = (event) => {
         setSelectedFile(event.target.files[0]);
     };
@@ -15,25 +32,52 @@ const FileUpload = () => {
             // z.B. Fetch oder eine AJAX-Bibliothek wie Axios
 
             // bsp mit Fetch:
-            fetch('/upload', {
+            fetch('http://localhost:8000/upload', {
                 method: 'POST',
-                body: formData
+                body: formData,
             })
-                .then(response => {
-                    // Handle die Serverantwort
+                .then((response) => response.json())
+                .then((data) => {
+                    console.log(data); // Serverantwort
+                    // Aktualisiere die Liste der hochgeladenen Dateien
+                    fetchUploadedFiles();
                 })
-                .catch(error => {
+                .catch((error) => {
                     // Handle den Fehler
+                    console.error(error);
                 });
-        }
-    };
+            }
+        };
+        const handleDelete = (fileName) => {
+            fetch(`http://localhost:8000/delete/${fileName}`, {
+                method: 'DELETE',
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    console.log(data); // Serverantwort
+                    // Aktualisiere die Liste der hochgeladenen Dateien
+                    fetchUploadedFiles();
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
 
-    return (
-        <div>
-            <input type="file" onChange={handleFileChange} />
-            <button onClick={handleUpload}>Hochladen</button>
-        </div>
-    );
-};
+        };
+
+        return (
+            <div>
+                <input type="file" onChange={handleFileChange}/>
+                <button onClick={handleUpload}>Hochladen</button>
+                <h2>Hochgeladene Dateien:</h2>
+                <ul>
+                    {uploadedFiles.map((file) => (
+                        <li key={file}>{file}
+                            <button onClick={() => handleDelete(file)}>Löschen</button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        );
+    };
 
 export default FileUpload;
