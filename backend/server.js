1889410e1310c4641076c3230ff1b1e9f7a9f66c
tukait02 <tukait02@hs-esslingen.de>
2023-05-31 10:33:21 +0200

    mehrere files hochladen

--- backend/server.js
+++ backend/server.js
@@ -8,24 +8,37 @@ const app = express();
 const upload = multer({ dest: 'uploads/' });
 
 app.use(cors());
-app.post('/upload', upload.single('file'), (req, res) => {
-    const originalName = req.file.originalname;
-    const fileName = Date.now() + '-' + originalName;
+app.post('/upload', upload.array('files', 10), (req, res) => {
+    const files =req.files;
+    const uploadPromises = files.map((file) => {
+        const originalName = file.originalname;
+        const fileName = Date.now() + '-' + originalName;
 
-    const filePath = req.file.path;
-    const targetPath = 'uploads/' + fileName;
+        const filePath = file.path;
+        const targetPath = 'uploads/' + fileName;
 
-    fs.rename(filePath, targetPath, (err) => {
-        if (err) {
-            console.error('Fehler beim Speichern der Datei: ', err);
-            res.status(500).send('Fehler beim Speichern der Datei.');
-        } else {
-            console.log('Datei erfolgreich gespeichert.');
-            res.send('Datei hochgeladen und gespeichert.');
-        }
-    });
-});
-
+        return new Promise((resolve, reject) => {
+            fs.rename(filePath, targetPath, (err) => {
+                if (err) {
+                console.error('Fehler beim Speichern der Datei: ', err);
+                reject(err);
+                } else {
+                    console.log('Datei erfolgreich gespeichert.', fileName);
+                    resolve(fileName);
+                }
+        });
+        });
+        });
+        Promise.all(uploadPromises)
+            .then((fileNames) => {
+                console.log('Alle Dateien erfolgreich hochgeladen.');
+                res.json({ fileNames });
+            })
+            .catch((error) => {
+             console.error('Fehler beim Hochladen der Dateien: ', error);
+             res.status(500).send('Fehler beim Hochladen der Dateien.');
+        });
+        });
 app.get('/files', (req, res) => {
     fs.readdir('uploads/', (err, files) => {
         if (err) {
@@ -46,7 +59,7 @@ app.delete('/delete/:fileName', (req, res) => {
             console.error('Fehler beim Löschen der Datei: ', err);
             res.status(500).send('Fehler beim Löschen der Datei.');
         } else {
-            console.log('Datei erfolgreich gelöscht.');
+            console.log('Datei erfolgreich gelöscht.', fileName);
             res.send('Datei gelöscht.');
         }
     });
