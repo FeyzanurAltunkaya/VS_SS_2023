53cefbce3fbb2825ccd37fa171ed2f5fba3e2475
aljait03 <aljait03@hs-esslingen.de>
2023-06-04 22:28:23 +0200

    cleanup

--- backend/src/main/java/com/example/projectFiler/DockerPostgresSpringbootApplication.java
+++ backend/src/main/java/com/example/projectFiler/DockerPostgresSpringbootApplication.java
@@ -2,8 +2,12 @@ package com.example.projectFiler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.example.projectFiler.repository")
+@EntityScan("com.example.projectFiler.entity")
 public class DockerPostgresSpringbootApplication {
 
   public static void main(String[] args) {
