0a68cc0b7b638543cb276262f51ff0e6497b8d92
fekoit00 <fekoit00@hs-esslingen.de>
2023-06-07 21:41:43 +0200

    FileUpload erneuert

--- frontend/src/pages/FileUpload.js
+++ frontend/src/pages/FileUpload.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { faUpload } from "@fortawesome/free-solid-svg-icons";
+import { faUpload, faFolder } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const FileUpload = () => {
     const [selectedFiles, setSelectedFiles] = useState([]);
     const [uploadedFiles, setUploadedFiles] = useState([]);
+    const [folders, setFolders] = useState([]);
+    const [activeFolder, setActiveFolder] = useState('');
 
     const fetchUploadedFiles = () => {
         fetch('http://localhost:8000/files')
@@ -16,10 +19,22 @@ const FileUpload = () => {
             });
     };
 
+    const fetchFolders = () => {
+        fetch('http://localhost:8000/folders')
+            .then((response) => response.json())
+            .then((data) => {
+                setFolders(data.folders);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    };
+
     useEffect(() => {
-        // Funktion aufrufen, um die hochgeladenen Dateien abrufen zu können
         fetchUploadedFiles();
+        fetchFolders();
     }, []);
+
     const handleFileChange = (event) => {
         setSelectedFiles(Array.from(event.target.files));
     };
@@ -27,13 +42,11 @@ const FileUpload = () => {
     const handleUpload = () => {
         if (selectedFiles.length > 0) {
             const formData = new FormData();
-            selectedFiles.forEach((file) =>{
+            selectedFiles.forEach((file) => {
                 formData.append('files', file);
+                formData.append('folder', activeFolder);
             });
-            // Hier den Upload-Request an den Server senden!!
-            // z.B. Fetch oder eine AJAX-Bibliothek wie Axios
 
-            // bsp mit Fetch:
             fetch('http://localhost:8000/upload', {
                 method: 'POST',
                 body: formData,
@@ -41,62 +54,132 @@ const FileUpload = () => {
                 .then((response) => response.json())
                 .then((data) => {
                     console.log(data); // Serverantwort
-                    // Aktualisiere die Liste der hochgeladenen Dateien
                     fetchUploadedFiles();
                 })
                 .catch((error) => {
-                    // Handle den Fehler
                     console.error(error);
                 });
-            }
-        };
-        const handleDelete = (fileName) => {
-            fetch(`http://localhost:8000/delete/${fileName}`, {
-                method: 'DELETE',
+        }
+    };
+
+    const handleDelete = (fileName) => {
+        fetch(`http://localhost:8000/delete/${fileName}`, {
+            method: 'DELETE',
+        })
+            .then((response) => response.text())
+            .then((data) => {
+                console.log(data); // Serverantwort
+                fetchUploadedFiles();
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    };
+
+    const handleCreateFolder = () => {
+        const folderName = prompt('Geben Sie den Namen des Ordners ein:');
+        if (folderName) {
+            fetch('http://localhost:8000/create-folder', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ folderName }),
             })
                 .then((response) => response.text())
                 .then((data) => {
                     console.log(data); // Serverantwort
-                    // Aktualisiere die Liste der hochgeladenen Dateien
-                    fetchUploadedFiles();
+                    fetchFolders();
                 })
                 .catch((error) => {
                     console.error(error);
                 });
+        }
+    };
 
-        };
+
+    const handleDragOver = (event) => {
+        event.preventDefault();
+    };
+
+    const handleDrop = (event, folder) => {
+        event.preventDefault();
+        const files = Array.from(event.dataTransfer.files);
+        const formData = new FormData();
+        files.forEach((file) => {
+            formData.append('files', file);
+            formData.append('folder', folder);
+        });
+
+        fetch('http://localhost:8000/upload', {
+            method: 'POST',
+            body: formData,
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data); // Serverantwort
+                fetchUploadedFiles();
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    };
+
+    const handleFolderClick = (folder) => {
+        setActiveFolder(folder);
+    };
 
     return (
         <div>
             <div className="your-directories">
-
-            <input
-                type="file"
-                onChange={handleFileChange}
-                className="file-input-container"
-                multiple
-            />
+                <input
+                    type="file"
+                    onChange={handleFileChange}
+                    className="file-input-container"
+                    multiple
+                />
                 <button onClick={handleUpload} className="upload-button">
                     <div className="upload-icon-container">
-                    <FontAwesomeIcon icon={faUpload} />
-                        </div>
+                        <FontAwesomeIcon icon={faUpload} />
+                    </div>
                 </button>
 
                 <h2>Hochgeladene Dateien:</h2>
-            <ul className="file-list">
-                {uploadedFiles.map((file) => (
-                    <li key={file} className="file-item">
-                        {file}
-                        <button onClick={() => handleDelete(file)}>Löschen</button>
-                    </li>
-                ))}
+                <ul className="file-list">
+                    {uploadedFiles.map((file) => (
+                        <li key={file} className="file-item">
+                            {file}
+                            <button onClick={() => handleDelete(file)}>Löschen</button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
 
-            </ul>
+            <div className="your-folders">
+                <h2>Ordner:</h2>
+                <ul className="folder-list">
+                    {folders.map((folder) => (
+                        <li
+                            key={folder}
+                            className={`folder-item ${
+                                activeFolder === folder ? 'active' : ''
+                            }`}
+                            onClick={() => handleFolderClick(folder)}
+                            onDragOver={handleDragOver}
+                            onDrop={(event) => handleDrop(event, folder)}
+                        >
+                            <FontAwesomeIcon icon={faFolder} />
+                            {folder}
+                        </li>
+                    ))}
+                </ul>
+                <div className="create-folder">
+                    <input type="text" placeholder="Ordnername" />
+                    <button onClick={handleCreateFolder}>Ordner erstellen</button>
+                </div>
             </div>
         </div>
-
     );
-
 };
 
 export default FileUpload;
