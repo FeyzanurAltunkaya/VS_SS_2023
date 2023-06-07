8f5a5ee391e1fc6e27ccf5e755cbb4b045253066
aljait03 <aljait03@hs-esslingen.de>
2023-06-07 17:07:09 +0200

    add many to many relationship for group

--- backend/src/main/java/com/example/projectFiler/entity/UserEntity.java
+++ backend/src/main/java/com/example/projectFiler/entity/UserEntity.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import java.util.HashSet;
+import java.util.Set;
 import lombok.*;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
@@ -33,4 +35,12 @@ public class UserEntity {
 
   //@Column(name = "is_admin", nullable = false)
   private boolean isAdmin = false;
+
+  @ManyToMany
+  @JoinTable(
+    name = "user_group",
+    joinColumns = @JoinColumn(name = "user_id"),
+    inverseJoinColumns = @JoinColumn(name = "group_id")
+  )
+  private Set<GroupEntity> groups = new HashSet<>();
 }
