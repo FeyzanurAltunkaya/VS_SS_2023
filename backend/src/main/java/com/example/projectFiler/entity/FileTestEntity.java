7db2cd2420fcaf8911e3e598abc8b648e5240dd1
aljait03 <aljait03@hs-esslingen.de>
2023-06-09 16:09:45 +0200

    update FileTestEntity

--- backend/src/main/java/com/example/projectFiler/entity/FileTestEntity.java
+++ backend/src/main/java/com/example/projectFiler/entity/FileTestEntity.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 @Setter
@@ -20,8 +21,6 @@ public class FileTestEntity {
   private Long id;
 
   private String name;
-
-  private String fileType;
 
   @Lob
   @Column(length = 10485760) // Adjust the length based on your database's maximum size limit for storing binary data
