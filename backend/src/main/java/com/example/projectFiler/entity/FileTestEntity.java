5907a82de35e601b31f24c923ebf4dbc17e81f6d
aljait03 <aljait03@hs-esslingen.de>
2023-06-09 13:22:06 +0200

    add filetestentitiy, first version, will get cleaned up later

---
+++ backend/src/main/java/com/example/projectFiler/entity/FileTestEntity.java
@@ -0,0 +1,29 @@
+package com.example.projectFiler.entity;
+
+import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@EnableAutoConfiguration
+public class FileTestEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  private String name;
+
+  private String fileType;
+
+  @Lob
+  @Column(length = 10485760) // Adjust the length based on your database's maximum size limit for storing binary data
+  private byte[] data;
+}
