9bf0c5387c9aaec87fb18219f3ed00fa7d7d294c
tukait02 <tukait02@hs-esslingen.de>
2023-06-10 19:08:31 +0200

    test für userrepository test wurde angelegt und erfolgreich durchgeführt

---
+++ backend/src/test/java/com/example/projectFiler/repository/UserRepositoryTest.java
@@ -0,0 +1,76 @@
+package com.example.projectFiler.repository;
+
+import com.example.projectFiler.entity.UserEntity;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class UserRepositoryTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserRepositoryTest userRepositoryTest;
+
+    @Test
+    void findByUsername_ExistingUser_ReturnsOptionalUserEntity() {
+        //  Variable username erstellen, als Eingabewert für den Testfall
+        String username = "testUser";
+        UserEntity expectedUserEntity = new UserEntity();
+        expectedUserEntity.setUsername(username);
+        when(userRepository.findByUsername(username)).thenReturn(Optional.of(expectedUserEntity));
+
+        // Ergebnis in Variable speichern
+        Optional<UserEntity> actualUserEntity = userRepository.findByUsername(username);
+
+        // Assert, ob das UserEntity objekt vorhanden ist
+        assertTrue(actualUserEntity.isPresent());
+        assertEquals(expectedUserEntity, actualUserEntity.get());
+        verify(userRepository, times(1)).findByUsername(username);
+    }
+
+    @Test
+    void findByUsername_NonExistingUser_ReturnsEmptyOptional() {
+        String username = "nonExistingUser";
+        when(userRepository.findByUsername(username)).thenReturn(Optional.empty());
+
+        Optional<UserEntity> actualUserEntity = userRepository.findByUsername(username);
+
+        // Überprüfung ob actualUserEntity Objekt leer ist
+        assertFalse(actualUserEntity.isPresent());
+        verify(userRepository, times(1)).findByUsername(username);
+    }
+
+    @Test
+    void existsByUsername_ExistingUser_ReturnsTrue() {
+        String username = "testUser";
+        when(userRepository.existsByUsername(username)).thenReturn(true);
+
+        boolean exists = userRepository.existsByUsername(username);
+
+        // Existiert username?
+        assertTrue(exists);
+        verify(userRepository, times(1)).existsByUsername(username);
+    }
+
+    @Test
+    void existsByUsername_NonExistingUser_ReturnsFalse() {
+        String username = "nonExistingUser";
+        when(userRepository.existsByUsername(username)).thenReturn(false);
+
+        boolean exists = userRepository.existsByUsername(username);
+
+        // überprüfen ob username nicht existiert?
+        assertFalse(exists);
+        verify(userRepository, times(1)).existsByUsername(username);
+    }
+}
