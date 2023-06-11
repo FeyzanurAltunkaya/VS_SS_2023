7cc7fc08c3060b89b98849c31eaf1da62f8fc387
fekoit00 <fekoit00@hs-esslingen.de>
2023-06-11 19:49:48 +0200

    Group Entity test, set und get name auskommentiert

--- backend/src/test/java/com/example/projectFiler/entity/GroupEntityTest.java
+++ backend/src/test/java/com/example/projectFiler/entity/GroupEntityTest.java
@@ -1,1 +1,56 @@
 package com.example.projectFiler.entity;
+import com.example.projectFiler.entity.GroupEntity;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GroupEntityTest {
+
+   /* @Test
+    void testGetName() {
+        // Create a new group entity
+        GroupEntity group = new GroupEntity();
+        group.setName("TestGroup");
+
+        // Verify the group's name
+        assertEquals("TestGroup", group.getName());
+    }*/
+
+    @Test
+    void testGetId() {
+        // Create a new group entity
+        GroupEntity group = new GroupEntity();
+        group.setId(1L);
+
+        // Verify the group's ID
+        assertEquals(1L, group.getId());
+    }
+
+    @Test
+    void testSetId() {
+        // Create a new group entity
+        GroupEntity group = new GroupEntity();
+        group.setId(1L);
+
+        // Verify the group's ID
+        assertEquals(1L, group.getId());
+
+        // Set a new ID
+        group.setId(2L);
+
+        // Verify the updated ID
+        assertEquals(2L, group.getId());
+    }
+
+    /*@Test
+    void testSetName() {
+        // Create a new group entity
+        GroupEntity group = new GroupEntity();
+
+        // Set the group's name using the setName() method
+        group.setName("TestGroup");
+
+        // Verify the group's name
+        assertEquals("TestGroup", group.getName());
+    }*/
+}
